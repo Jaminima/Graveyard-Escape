58c6aa2c0200f370480c525a8ed714697971f63e
Oscar Davies <oscar@designlogica.co.uk>
2025-01-05 00:04:16 +0000

    Size the new element by timing mouse hold

--- Graveyard_Escape_Game/GL/Window.cs
+++ Graveyard_Escape_Game/GL/Window.cs
@@ -102,6 +102,8 @@ namespace Graveyard_Escape_Game
             _world.Update((float)e.Time);
         }
 
+        private float mouseHeldFor = 0.0f;
+        private Random random = new Random();
         private void HandleInput(float deltaTime)
         {
             KeyboardState keyboardState = KeyboardState.GetSnapshot();
@@ -137,17 +139,32 @@ namespace Graveyard_Escape_Game
                 _zoom -= zoomStep;
             }
 
-            if (mouseState.IsButtonPressed(MouseButton.Left))
+            if (mouseState.IsButtonDown(MouseButton.Left))
+            {
+                mouseHeldFor += deltaTime;
+            }
+
+            if (mouseState.IsButtonReleased(MouseButton.Left))
             {
                 System.Numerics.Vector2 mousePosition = new System.Numerics.Vector2(mouseState.X, mouseState.Y);
                 System.Numerics.Vector2 worldPosition = new System.Numerics.Vector2(mousePosition.X / _width, mousePosition.Y / _height);
                 worldPosition = new System.Numerics.Vector2(worldPosition.X * 2 - 1, 1 - worldPosition.Y * 2);
                 worldPosition = new System.Numerics.Vector2(worldPosition.X / _zoom, worldPosition.Y / _zoom);
                 worldPosition += _cameraPosition;
-                var entity= new Entity<EntityRenderer>() { Id = _world.maxEntityId, Position = new System.Numerics.Vector2(worldPosition.X, worldPosition.Y), Radius=0.001f, SpinSpeed = 0, Velocity = new System.Numerics.Vector2(0,0), Colour = new System.Numerics.Vector4(1.0f, 1.0f, 1.0f, 1.0f) };
+
+                float radius = mouseHeldFor * 0.01f;
+                float mass = mouseHeldFor * 10.0f ;
+
+                float r = random.Next(0, 255) / 255.0f;
+                float g = random.Next(0, 255) / 255.0f;
+                float b = random.Next(0, 255) / 255.0f;
+
+                var entity= new Entity<EntityRenderer>() { Id = _world.maxEntityId, Position = new System.Numerics.Vector2(worldPosition.X, worldPosition.Y), Radius=radius, Mass = mass, SpinSpeed = 0, Velocity = new System.Numerics.Vector2(0,0), Colour = new System.Numerics.Vector4(r, g, b, 1.0f) };
                 entity.Init();
                 _world.Entities.Add(entity);
                 _world.maxEntityId++;
+
+                mouseHeldFor = 0.0f;
             }
         }
 
